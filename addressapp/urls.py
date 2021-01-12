231ad3288c54198da41783db27c3d78e307a8ae4
hovak <hovak@kakao.com>
2021-01-13 04:58:18 +0900

    addresss create bug fixed and include base.js for csrf token

--- addressapp/urls.py
+++ addressapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from addressapp.views import AddressCreateView, AddressListView
+from addressapp.views import AddressCreateView, AddressListView, AddressUpdateDefaultView
 
 app_name = 'addressapp'
 
@@ -8,4 +8,5 @@ app_name = 'addressapp'
 urlpatterns = [
     path('create/', AddressCreateView.as_view(), name='create'),
     path('list/', AddressListView.as_view(), name='list'),
+    path('update/<int:pk>/', AddressUpdateDefaultView.as_view(), name='update')
 ]
