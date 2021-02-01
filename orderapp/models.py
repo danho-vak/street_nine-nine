f7878291b9ec05f4171215cd0a793a5f253f4192
danho-vak <hovak@kakao.com>
2021-02-01 17:45:21 +0900

    create iamport.py in orderapp but is not applied

--- orderapp/models.py
+++ orderapp/models.py
@@ -11,6 +11,10 @@ class Order(models.Model):
     address = models.ForeignKey(UserAddress, on_delete=models.SET_NULL, related_name='order_address', null=True)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='order_cart')
     total_price = models.IntegerField(null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
 
     def delivery_charge(self):
         if self.cart.total_price() >= 50000:
